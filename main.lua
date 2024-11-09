507ba7116b1aa73773c2979aedf42159c31efa0a
chris <armoredmoose@gmail.com>
2024-11-08 21:47:07 -0600

    classes are wirrent to files in classes directory

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ local function main()
 	local argsTable <const> = ArgReader:new().readArgs(arg)
 	local classData <const> = DataParser:new().parseData(argsTable)
 	local class <const> = ClassBuilder:new().buildClass(classData)
-	local file <const> = io.open(classData.className .. ".lua","w+")
+	local file <const> = io.open("./classes/" .. classData.className .. ".lua","w+")
 	file:write(class)
 	file:close()
 end
