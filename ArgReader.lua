c7b17904e92a83db98d7f8b9b066f47ecce55276
chris <armoredmoose@gmail.com>
2024-11-08 20:10:05 -0600

    added ArgReader.

---
+++ ArgReader.lua
@@ -0,0 +1,33 @@
+local setmetatable <const> = setmetatable
+local gmatch <const> = string.gmatch
+local concat <const> = table.concat
+
+local ArgReader <const> = {__type="ArgReader"}
+ArgReader.__index = ArgReader
+
+_ENV = ArgReader
+
+local function loopOverOptions(flag,options,argsTable)
+	local tbl <const> = {}
+	if options and options ~= "" then
+		for opt in gmatch(options,"([^,]+)") do
+			tbl[#tbl + 1] = opt
+		end
+	end
+	argsTable[flag] = tbl
+end
+
+function ArgReader.readArgs(args)
+	local argsTable <const> = {}
+	local argStr <const> = concat(args," ")
+	for flag, options in gmatch(argStr,"-(%S*)%s*(%S*)") do
+		loopOverOptions(flag,options,argsTable)
+	end
+	return argsTable
+end
+
+function ArgReader:new()
+	return setmetatable({},self)
+end
+
+return ArgReader
---
+++ main.lua
@@ -0,0 +1,8 @@
+
+local ArgReader <const> = require('ArgReader')
+
+local function main()
+	local argsTable <const> = ArgReader:new().readArgs(arg)
+end
+
+main()
