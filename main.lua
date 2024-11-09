1eda3399fec42f1d6c3cef0e781b886d927d5a23
chris <armoredmoose@gmail.com>
2024-11-09 17:54:36 -0600

    updated preamble

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 --[[
 	Class Generator. command line tool to generate skeleton of a lua class.className
+	Copyright (C) <2024> github/return5
 
 	This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, exclusively version 3 of the License.
 
